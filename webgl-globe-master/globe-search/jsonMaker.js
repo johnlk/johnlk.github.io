ee28214d84690e92fec98e77689b663b2b36d04c
John Kuhn <johnkuhn@Johns-MacBook-Pro.local>
2016-10-25 17:27:49 -0400

    added comments

--- webgl-globe-master/globe-search/jsonMaker.js
+++ webgl-globe-master/globe-search/jsonMaker.js
@@ -2,16 +2,20 @@
 
 //https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat
 
+//will hold the country colors
 var countries = {
     
 };
 
+//holds the info regarding flight detinations and trips
 var flights = new Array(67664);
 
 $.get('https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat', function(data){
+    
     var holder = data.split('\n');
     var line;
     var subArray = [];
+    
     for(var i = 0; i < holder.length; i++){
         line = holder[i].split(',');
         subArray = new Array(2);
@@ -19,13 +23,13 @@ $.get('https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes
         subArray[1] = line[4];
         flights[i] = subArray;
     }
-    //console.log(flights[500]);
-    //console.log(countFlights('ATL'));
+    
 });
 
 
 //console.log(countFlights('GKA'));
 
+//returns the number of flights an airport sees
 function countFlights(airportAbriviation){
     var sum = 0;
     for(var i = 0; i < flights.length; i++){
@@ -39,6 +43,7 @@ function countFlights(airportAbriviation){
     return sum;
 }
 
+//if a country already has been assigned a color return it, else assign a country color
 function getColor(country){
     if(countries[country] == undefined){
         countries[country] = Math.floor(Math.random() * 23) + 1;
@@ -49,25 +54,25 @@ function getColor(country){
 //busiest airpot ATL, flights 1826 per day, this will be max
 
 $.get('https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat', function(data) {
+    
     var arr = data.split('\n');
     var line;    
     
     setTimeout(function(){
         document.write('[');
-        //for(var i = 0; i < 100; i++){
         for(var i = 0; i < arr.length-1; i++){
             line = arr[i].split(',');
             document.write(line[6] + ',');
             document.write(line[7] + ',');
             document.write(((countFlights(line[4].substring(1, line[4].length-1))/3652) + 0.001).toFixed(6) + ',');
-            document.write(getColor(line[3].substring(1, line[3].length-1)));
-            //if(i < 99){
+            document.write(getColor(line[3].substring(1, line[3].length-1)));\
             if(i < arr.length - 2){
                 document.write(',');
             }
         }
         document.write(']');
-        console.log(countries);
     },10); 
+    
+    //json file will contain lat, long, magnitude, color
     
 });
