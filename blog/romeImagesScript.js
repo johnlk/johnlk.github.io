881652d991a4f3681b764edf72cc4899faaaf6c7
johnlk <jlkuhn@purdue.edu>
2018-10-29 21:23:59 +0100

    removed the image directory and put it on an s3 bucket

--- blog/romeImagesScript.js
+++ blog/romeImagesScript.js
@@ -1,15 +1,14 @@
-        $(document).ready(function(){
-            var gallery_row = $('.row');
-            var images = "IMG_0582.JPG IMG_0592.JPG IMG_0593.JPG IMG_0596.JPG IMG_0597.JPG IMG_0602.JPG IMG_0610.JPG IMG_0611.JPG IMG_0615.JPG IMG_0616.JPG IMG_0621.JPG IMG_0624.JPG IMG_0625.JPG IMG_0628.JPG IMG_0632.JPG IMG_0640.JPG IMG_0641.JPG IMG_0652.JPG IMG_0657.JPG IMG_0658.JPG IMG_0661.JPG IMG_0667.JPG IMG_0673.JPG IMG_0674.JPG IMG_0677.JPG IMG_0678.JPG IMG_0679.JPG IMG_0682.JPG IMG_0693.JPG IMG_0695.JPG IMG_0704.JPG IMG_0706.JPG IMG_0712.JPG IMG_0719.JPG IMG_0727.JPG IMG_0736.JPG".split(' ');
-            for(i in images){
-                var image = images[i];
-                image = image.replace('.JPG', '_low.JPG');
-                gallery_row
-                    .append('<div class="col-sm-6 col-md-4">' +
-                                '<a class="lightbox" href="../images/' + image + '">' +
-                                    '<img src="../images/' + image + '" >' +
-                                '</a>' +
-                            '</div>');
-            }
-            baguetteBox.run('.tz-gallery');
-        });
+$(document).ready(function(){
+  var gallery_row = $('.row');
+  var images = "img_0582_low.JPG img_0592_low.JPG img_0593_low.JPG img_0596_low.JPG img_0597_low.JPG img_0602_low.JPG img_0610_low.JPG img_0611_low.JPG img_0615_low.JPG img_0616_low.JPG img_0621_low.JPG img_0624_low.JPG img_0625_low.JPG img_0628_low.JPG img_0632_low.JPG img_0640_low.JPG img_0641_low.JPG img_0652_low.JPG img_0657_low.JPG img_0658_low.JPG img_0661_low.JPG img_0667_low.JPG img_0673_low.JPG img_0674_low.JPG img_0677_low.JPG img_0678_low.JPG img_0679_low.JPG img_0682_low.JPG img_0693_low.JPG img_0695_low.JPG img_0704_low.JPG img_0706_low.JPG img_0712_low.JPG img_0719_low.JPG img_0727_low.JPG img_0736_low.JPG".split(' ');
+  for(i in images){
+    var image = images[i];
+    gallery_row
+      .append('<div class="col-sm-6 col-md-4">' +
+                '<a class="lightbox" href="https://s3.amazonaws.com/blog123videos/images/' + image + '">' +
+                    '<img src="https://s3.amazonaws.com/blog123videos/images/' + image + '" >' +
+                '</a>' +
+              '</div>');
+  }
+  baguetteBox.run('.tz-gallery');
+});
